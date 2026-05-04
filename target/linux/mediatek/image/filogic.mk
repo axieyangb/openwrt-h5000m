ea21270ce4e539ccbf35045c5a15257efbf21890
yangyang xie <axieyangb@yangyangs-Mac-mini.local>
2026-05-03 19:02:17 -0700

    fix: upgrade default wpad to mesh-mbedtls at target level to resolve rootfs conflict

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -1755,7 +1755,6 @@ define Device/hiveton_h5000m
 	kmod-mt7996e kmod-mt7992-23-firmware e2fsprogs f2fsck mkf2fs \
 	kmod-usb-net-qmi-wwan kmod-usb-serial-option uqmi luci-proto-qmi usbutils \
 	kmod-nft-offload luci luci-app-firewall luci-theme-bootstrap \
-	-wpad-basic-mbedtls wpad-mesh-mbedtls \
 	wireguard-tools luci-proto-wireguard kmod-tun kmod-tcp-bbr luci-app-sqm \
 	pciutils htop bash kmod-fs-ntfs3 kmod-fs-exfat
   KERNEL_LOADADDR := 0x40080000
